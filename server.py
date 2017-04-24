86daeaec024f609be2b4e6342d013f21fdf3bbe0
Glen Baker <iepathos@gmail.com>
2017-04-23 20:32:12 -0400

    Added test, fixed bugs with view, added curl example

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import logging
 from textblob import TextBlob
-from flask import Flask, request
+from flask import Flask, request, jsonify
+logger = logging.getLogger('marlowe.objectivity.server')
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
     text = TextBlob(request.form.get('text'))
     objectivity = 1.0 - text.sentiment.subjectivity
-    return {'objectivity': objectivity}
+    data = {'objectivity': objectivity}
+    return jsonify(data)
 
 
 if __name__ == '__main__':
